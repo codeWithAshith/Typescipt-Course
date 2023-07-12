3add60424e25e9716a9a8337172871ebe64cba7d
codeWithAshith <codewithashith@gmail.com>
2023-07-12 10:33:42 +0530

    Added project

---
+++ 01. Maps/index.ts
@@ -0,0 +1,4 @@
+console.log("Hello World");
+
+// https://fakerjs.dev/
+// npm install @faker-js/faker --save-dev
---
+++ 01. Maps/src/CustomMap.ts
@@ -0,0 +1,50 @@
+// enable google map javascript api
+// generate credentails - api key
+
+// added map as script which adds "google" as a js global variable
+// To help ts understand -> install
+// npm install --save @types/google.maps
+// Below line is important
+
+/// <reference types="@types/google.maps" />
+
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+  color: string;
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId)!, {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
